09808c0cde649500d1fab9f13035e06bf87fff24
sauraml <saurabhrawatml@gmail.com>
2025-03-28 01:10:13 +0530

    completed parsing skipped in parseLetStatement and parseReturnStatement

--- parser/parser.go
+++ parser/parser.go
@@ -154,9 +154,11 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 		return nil
 	}
 
-	// Skipping the expressions until we
-	// encounter a semicolon
-	for !p.curTokenIs(token.SEMICOLON) {
+	p.nextToken()
+
+	stmt.Value = p.parseExpression(LOWEST)
+
+	if p.peekTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
 
@@ -168,8 +170,8 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 
 	p.nextToken()
 
-	// Skipping the expressions until we
-	// encounter a semicolon
+	stmt.ReturnValue = p.parseExpression(LOWEST)
+
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
 	}
