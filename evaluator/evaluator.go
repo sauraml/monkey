44a55330034c10030fcf73c3bbb1e22b9e42d0b1
sauraml <saurabhrawatml@gmail.com>
2025-04-04 11:54:57 +0530

    add return satement evaluation

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -13,14 +13,18 @@ var (
 
 func Eval(node ast.Node) object.Object {
 	switch node := node.(type) {
+
 	// Satements
 	case *ast.Program:
-		return evalStatements(node.Statements)
+		return evalProgram(node)
+
+	case *ast.BlockStatement:
+		return evalBlockStatement(node)
 
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
 
-		// Expressions
+	// Expressions
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 
@@ -36,21 +40,40 @@ func Eval(node ast.Node) object.Object {
 		right := Eval(node.Right)
 		return evalInfixExpression(node.Operator, left, right)
 
-	case *ast.BlockStatement:
-		return evalStatements(node.Statements)
-
 	case *ast.IfExpression:
 		return evalIfExpression(node)
+
+	case *ast.ReturnStatement:
+		val := Eval(node.ReturnValue)
+		return &object.ReturnValue{Value: val}
 	}
 
 	return nil
 }
 
-func evalStatements(statements []ast.Statement) object.Object {
+func evalProgram(program *ast.Program) object.Object {
 	var result object.Object
 
-	for _, statement := range statements {
+	for _, statement := range program.Statements {
 		result = Eval(statement)
+
+		if returnValue, ok := result.(*object.ReturnValue); ok {
+			return returnValue.Value
+		}
+	}
+
+	return result
+}
+
+func evalBlockStatement(block *ast.BlockStatement) object.Object {
+	var result object.Object
+
+	for _, statement := range block.Statements {
+		result = Eval(statement)
+
+		if result != nil && result.Type() == object.RETURN_VALUE_OBJ {
+			return result
+		}
 	}
 
 	return result
