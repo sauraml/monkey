f2b270f6a246cefef5d6264736ab4a27b1106a3f
sauraml <saurabhrawatml@gmail.com>
2025-04-09 12:43:40 +0530

    added 'print' builtin function

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,6 +1,8 @@
 package evaluator
 
 import (
+	"fmt"
+
 	"github.com/sauraml/monkey/object"
 )
 
@@ -101,6 +103,15 @@ var builtins = map[string]*object.Builtin{
 			newElements[length] = args[1]
 
 			return &object.Array{Elements: newElements}
+		},
+	},
+	"print": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }
